a772f20208412dbb38607ffdb65de54578f51950
chauhaidang1307 <chauhaidang06@gmail.com>
2025-03-22 13:21:44 +0700

    return to first state

--- dsatur-coloring-problem/dsatur.cpp
+++ dsatur-coloring-problem/dsatur.cpp
@@ -2,7 +2,5 @@
 using namespace std;
 int main(){
     cout << "Hello world!" << endl;
-    cout << "A change in github, test the Pull" << endl;
-    cout << "Test another commit and push to github :D" << endl;
     return 0;
 }
